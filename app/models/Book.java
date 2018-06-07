9164cb9e934eda82a57408dceb35723d60b304fe
vilenc3 <vilence@interia.pl>
2018-06-07 09:41:00 +0200

    bump

--- app/models/Book.java
+++ app/models/Book.java
@@ -6,30 +6,18 @@ import java.util.Set;
 
 public class Book {
     public Integer id;
-<<<<<<< HEAD
     public String link;
     public String title;
-=======
-    public String title;
-    public Integer price;
->>>>>>> ec555096eff22510aafdad743c0f3f357ff9bda5
     public String author;
 
     public Book(){
 
     }
 
-<<<<<<< HEAD
     public Book(Integer id, String link, String title, String author){
         this.id = id;
         this.link=link;
         this.title=title;
-=======
-    public Book(Integer id, String title, Integer price, String author){
-        this.id = id;
-        this.title=title;
-        this.price=price;
->>>>>>> ec555096eff22510aafdad743c0f3f357ff9bda5
         this.author=author;
     }
 
@@ -38,18 +26,9 @@ public class Book {
 
     static {
         books = new HashSet<>();
-<<<<<<< HEAD
         books.add(new Book(1, "https://picsum.photos/200/300?image=63", "Picture1", "Kot"));
         books.add(new Book(2, "https://picsum.photos/200/300?image=634", "Picture2", "Kot"));
-        books.add(new Book(3, "https://picsum.photos/200/300?image=13", "Picture3", "Kot"));
-        books.add(new Book(4, "https://picsum.photos/200/300?image=85", "Picture4", "Kot"));
-=======
-        cart = new HashSet<>();
-        books.add(new Book(1, "The Trial", 20, "Franz Kafka"));
-        books.add(new Book(2, "Silmarillion", 15, "J R R Tolkien"));
-        books.add(new Book(3, "The Odyssey", 25, "Homer"));
-        books.add(new Book(4, "War and Peace", 30, "Leo Tolstoy"));
->>>>>>> ec555096eff22510aafdad743c0f3f357ff9bda5
+        books.add(new Book(3, "https://picsum.photos/200/300?image=653", "Picture3", "Kot"));
     }
 
     public static Set<Book> retrieveAll(){
